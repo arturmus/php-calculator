3237d292c6ac5afc2552126cd811be9d2a78794b
arturmus <artur.musaelyan@gmail.com>
2020-04-14 17:06:26 +0300

    Calculate the sum of two parameters

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<head></head>
+    <body>
+    <h1>
+Calculator
+</h1>
+    <p>
+    <?php
+        $operation = $_GET['o'];
+        $a = $_GET['a'];
+        $b = $_GET['b'];
+if($operation !='sum'&&$operation !='sub'&&$operation !='div'&&$operation !='mul'){
+echo 'Unexpected operation';
+exit;
+}
+
+if(!is_numeric($a) || !is_numeric($b)){
+echo 'invalid arguments';
+exit;
+}     
+  
+if($operation == 'sum'){
+$result = $a + $b;
+}
+
+if($operation == 'sub'){
+$result = $a - $b;
+}
+
+if($operation == 'div'){
+$result = $a / $b;
+}
+
+if($operation == 'mul'){
+$result = $a * $b;
+}
+
+echo $operation, 'is', $result;
+?>
+
+    </p>
+    </body>
+
+</html>
